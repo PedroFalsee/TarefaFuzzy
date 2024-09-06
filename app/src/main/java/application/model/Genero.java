5445538bbd11674c70ac99ee3d59f3254692a928
PedroFalsee 123 <esphfm123@gmail.com>
2024-09-06 19:36:26 +0000

    tarefafuzzy2025

--- app/src/main/java/application/model/Genero.java
+++ app/src/main/java/application/model/Genero.java
@@ -1,6 +1,11 @@
 
 package application.model;
-import jakarta.persistence.Entity; import jakarta.persistence.GeneratedValue; import jakarta.persistence.GenerationType; import jakarta.persistence.Id; import jakarta.persistence.Table;
+
+import jakarta.persistence.Entity; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Table;
+
 @Entity
 @Table (name="generos")
 public class Genero {

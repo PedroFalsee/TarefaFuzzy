335c6978a85d12f0ffe072a39282e328f28688a1
PedroFalsee 123 <esphfm123@gmail.com>
2024-09-06 19:31:24 +0000

    tarefafuzzy2024

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -9,15 +9,16 @@ import application.model.GeneroRepository;
     @Controller
     @RequestMapping("/genero")
 public class GeneroController { 
-    
+
     @Autowired
     private GeneroRepository generoRepo;
 
     @RequestMapping("/list")
     public String list (Model model) {
-    }
+    
         model.addAttribute("generos", generoRepo.findAll());
         return "/genero/list";
+    }
         @RequestMapping("/insert")
         public String insert() {
         return "/genero/insert";
--- app/src/main/java/application/model/Genero.java
+++ app/src/main/java/application/model/Genero.java
@@ -7,16 +7,17 @@ public class Genero {
     @GeneratedValue (strategy=GenerationType. IDENTITY) 
     private int id;
     private String nome;
+
     public int getId() {
-    return id;
+        return id;
     }
     public void setId(int id) {
-    }
-    this.id = id;
+        this.id = id;
     }
     public String getNome() {
-    return nome;
+        return nome;
     }
     public void setNome(String nome) {
-    this.nome = nome;
+        this.nome = nome;
+    }
 }
